388d294f309599629aea737fe1729cc97f26cdc4
Alex Tituaña <aatituanau@uce.edu.ec>
2026-06-01 20:30:49 -0500

    fix: inject env variables and disable unused EC2s

--- infra/terraform/environments/qa/outputs.tf
+++ infra/terraform/environments/qa/outputs.tf
@@ -6,25 +6,25 @@ output "ms_core_private_ip" {
   value = module.ec2_2_ms_core.private_ip
 }
 
-output "ms_processing_private_ip" {
-  value = module.ec2_3_ms_processing.private_ip
-}
+# output "ms_processing_private_ip" {
+#   value = module.ec2_3_ms_processing.private_ip
+# }
 
-output "ms_tracking_private_ip" {
-  value = module.ec2_4_ms_tracking.private_ip
-}
+# output "ms_tracking_private_ip" {
+#   value = module.ec2_4_ms_tracking.private_ip
+# }
 
-output "ms_specialized_private_ip" {
-  value = module.ec2_5_ms_specialized.private_ip
-}
+# output "ms_specialized_private_ip" {
+#   value = module.ec2_5_ms_specialized.private_ip
+# }
 
 output "db_postgres_private_ip" {
   value = module.ec2_6_db_postgres.private_ip
 }
 
-output "db_mongodb_private_ip" {
-  value = module.ec2_7_db_mongodb.private_ip
-}
+# output "db_mongodb_private_ip" {
+#   value = module.ec2_7_db_mongodb.private_ip
+# }
 
 output "db_queues_private_ip" {
   value = module.ec2_8_db_queues.private_ip
