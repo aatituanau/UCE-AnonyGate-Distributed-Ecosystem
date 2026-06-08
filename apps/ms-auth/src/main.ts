78daa5911562239d867588e277fba61e3e7d09af
Alex Tituaña <aatituanau@uce.edu.ec>
2026-06-07 21:12:01 -0500

    feat: add frontend web client and configure microservices cors/dev scripts

--- apps/ms-auth/src/main.ts
+++ apps/ms-auth/src/main.ts
@@ -3,6 +3,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // Enable CORS for frontend integration
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  });
   await app.listen(process.env.PORT ?? 3000);
 }
 void bootstrap();
