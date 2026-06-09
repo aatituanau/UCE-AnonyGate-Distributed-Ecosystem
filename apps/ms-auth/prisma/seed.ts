899fe255f58449576ef115584fc4626addb0d734
Alex Tituaña <aatituanau@uce.edu.ec>
2026-06-08 23:07:38 -0500

    fix: isolate prisma clients for CI parallel build and fix bitnami tags

--- apps/ms-auth/prisma/seed.ts
+++ apps/ms-auth/prisma/seed.ts
@@ -1,5 +1,5 @@
 import 'dotenv/config';
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from '../src/generated/prisma';
 import * as bcrypt from 'bcrypt';
 import { Pool } from 'pg';
 import { PrismaPg } from '@prisma/adapter-pg';
