1294ca7c78163dbde934fb990603c3207617755a
Alex Tituaña <aatituanau@uce.edu.ec>
2026-06-11 18:43:43 -0500

    fix(swagger): update swagger paths to work behind nginx reverse proxy

--- apps/ms-alias/src/main.ts
+++ apps/ms-alias/src/main.ts
@@ -19,7 +19,7 @@ async function bootstrap() {
     .setVersion('1.0')
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api/docs', app, document);
+  SwaggerModule.setup('aliases/api/docs', app, document);
 
   // Connect gRPC microservice
   app.connectMicroservice<MicroserviceOptions>({
--- apps/ms-auth/src/main.ts
+++ apps/ms-auth/src/main.ts
@@ -18,7 +18,7 @@ async function bootstrap() {
     .addBearerAuth()
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api/docs', app, document);
+  SwaggerModule.setup('auth/api/docs', app, document);
 
   await app.listen(process.env.PORT ?? 3000);
 }
