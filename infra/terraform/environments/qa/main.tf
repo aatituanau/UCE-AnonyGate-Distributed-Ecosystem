0eb71967c1d0378f22588e1569d29ff8b14cd451
Alex Tituaña <aatituanau@uce.edu.ec>
2026-06-11 21:21:45 -0500

    style(infra): format terraform files

--- infra/terraform/environments/qa/main.tf
+++ infra/terraform/environments/qa/main.tf
@@ -1,6 +1,6 @@
 module "vpc" {
-  source              = "../../modules/vpc"
-  environment         = var.environment
+  source               = "../../modules/vpc"
+  environment          = var.environment
   vpc_cidr             = "10.1.0.0/16"
   public_subnet_cidrs  = ["10.1.1.0/24", "10.1.3.0/24"]
   private_subnet_cidrs = ["10.1.2.0/24", "10.1.4.0/24"]
