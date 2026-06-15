cf08a226855e02088e0004c14372ac1b6deada66
Alex Tituaña <aatituanau@uce.edu.ec>
2026-06-15 09:17:08 -0500

    fix: remove pg adapter to fix seed ECONNREFUSED

--- apps/ms-auth/prisma/seed.js
+++ apps/ms-auth/prisma/seed.js
@@ -1,13 +1,7 @@
 require('dotenv/config');
 const { PrismaClient } = require('../dist/generated/prisma');
 const bcrypt = require('bcrypt');
-const { Pool } = require('pg');
-const { PrismaPg } = require('@prisma/adapter-pg');
-
-const connectionString = process.env.DATABASE_URL;
-const pool = new Pool({ connectionString });
-const adapter = new PrismaPg({ pool });
-const prisma = new PrismaClient({ adapter });
+const prisma = new PrismaClient();
 
 // ─────────────────────────────────────────────────────────────────────────────
 // SEED DATA
